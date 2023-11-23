2f989a3d6c024336b01380f57374dfe1b0dbe68a
motiurrahman124 <motiurr503@gmail.com>
2023-11-23 11:49:46 +0600

    user interface done

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,35 @@
+import { Model } from 'mongoose';
+
+export type TUserFullName = {
+  firstName: string;
+  lastName: string;
+};
+
+export type TUserAddress = {
+  street: string;
+  city: string;
+  country: string;
+};
+
+export type TUserOrders = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+export type TUser = {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: TUserFullName;
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: TUserAddress;
+  orders?: TUserOrders;
+};
+
+export interface UserModel extends Model<TUser> {
+  isUserExists(userId: number): Promise<TUser | null>;
+}
